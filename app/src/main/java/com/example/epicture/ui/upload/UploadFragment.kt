6016f557e810c75046c06d92d09ac9e7ab9132e6
alexis <alexis.walter@epitech.eu>
2020-10-13 18:15:04 +0200

    feat(master): navigation

---
+++ app/src/main/java/com/example/epicture/ui/upload/UploadFragment.kt
@@ -0,0 +1,25 @@
+package com.example.epicture.ui.upload
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.epicture.R
+
+class UploadFragment : Fragment() {
+
+    private lateinit var uploadViewModel: UploadViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        uploadViewModel =
+                ViewModelProviders.of(this).get(UploadViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_upload, container, false)
+        return root
+    }
+}
