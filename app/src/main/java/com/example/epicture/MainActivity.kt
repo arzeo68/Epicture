7671ac56d79989fdae4e58f1cf57d73b109cd250
alexis <alexis.walter@epitech.eu>
2020-10-13 18:35:10 +0200

    feat(master): delete action bar

--- app/src/main/java/com/example/epicture/MainActivity.kt
+++ app/src/main/java/com/example/epicture/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home, R.id.navigation_profile, R.id.navigation_upload))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
