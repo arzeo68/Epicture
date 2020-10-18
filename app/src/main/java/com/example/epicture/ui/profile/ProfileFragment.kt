1fa8b8596837f8e6f04a81df6786743b34b6f720
alexis <alexis.walter@epitech.eu>
2020-10-18 15:38:40 +0200

    profile scene

--- app/src/main/java/com/example/epicture/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/epicture/ui/profile/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.example.epicture.ui.profile
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,14 +12,15 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.epicture.R
 import com.example.epicture.SettingsActivity
 
+
 class ProfileFragment : Fragment() {
 
     private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel::class.java)
@@ -28,6 +30,20 @@ class ProfileFragment : Fragment() {
             val intent = Intent(context, SettingsActivity::class.java)
             startActivity(intent)
         }
+        val buttonMyPicture: ImageButton = returnValue?.findViewById(R.id.myPictureButton)!!
+        val buttonLikedPicture: ImageButton = returnValue?.findViewById(R.id.likedPicture)!!
+        buttonLikedPicture.setColorFilter(Color.GRAY)
+        buttonMyPicture.setColorFilter(Color.WHITE)
+        buttonLikedPicture.setOnClickListener {
+            buttonMyPicture.setColorFilter(Color.GRAY)
+            buttonLikedPicture.setColorFilter(Color.WHITE)
+        }
+        buttonMyPicture.setOnClickListener {
+            buttonLikedPicture.setColorFilter(Color.GRAY)
+            buttonMyPicture.setColorFilter(Color.WHITE)
+
+        }
+
         return returnValue
     }
 }
