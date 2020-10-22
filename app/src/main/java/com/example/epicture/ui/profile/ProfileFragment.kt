4c18fb39906c3f958015cf35537485ffbb70a461
jgchoppe <jean-gael.choppe@epitech.eu>
2020-10-22 20:29:53 +0200

    feat(master): ux/ui profile

--- app/src/main/java/com/example/epicture/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/epicture/ui/profile/ProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.RelativeLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
