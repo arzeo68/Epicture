f145e2044db020967d3843aa346f170ee0a88dc0
alexis <alexis.walter@epitech.eu>
2020-10-22 23:20:59 +0200

    fix(master): home page

--- app/src/main/java/com/example/epicture/ui/home/MyAdapterHomePage.kt
+++ app/src/main/java/com/example/epicture/ui/home/MyAdapterHomePage.kt
@@ -5,16 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.epicture.R
-import com.example.epicture.http.Image
+import com.example.epicture.http.HomeGallery
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.home_view_list.view.*
 import kotlinx.android.synthetic.main.my_picture_view_list.view.*
+import kotlinx.android.synthetic.main.my_picture_view_list.view.imageTitle
+import kotlinx.android.synthetic.main.my_picture_view_list.view.likeButton
 
 
 class MyAdapterHomePage(
     private val context: Context,
-    private val dataSource: List<Image>?,
-    private val buttonLikeCallback: (String?, String) -> Unit
+    private val dataSource: List<HomeGallery>?,
+    private val buttonLikeCallback: (String?, String) -> Unit,
+    private val clickOnAlbumCallback: (String?) -> Unit
 ) : RecyclerView.Adapter<MyAdapterHomePage.MyViewHolder>() {
 
     private val inflater: LayoutInflater =
@@ -38,11 +42,11 @@ class MyAdapterHomePage(
     ): MyViewHolder {
         // create a new view
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.my_picture_view_list, parent, false)
+            .inflate(R.layout.home_view_list, parent, false)
         // set the view's size, margins, paddings and layout parameters
-        val scale: Float = context.getResources().getDisplayMetrics().density
-        val dpAsPixels = (32 * scale + 0.5f).toInt()
-        view.setPadding(view.paddingLeft, view.paddingTop, view.paddingRight, dpAsPixels);
+//        val scale: Float = context.getResources().getDisplayMetrics().density
+//        val dpAsPixels = (32 * scale + 0.5f).toInt()
+//        view.setPadding(view.paddingLeft, view.paddingTop, view.paddingRight, dpAsPixels);
         return MyViewHolder(view)
     }
 
@@ -51,13 +55,15 @@ class MyAdapterHomePage(
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         if (dataSource != null) {
-            holder.myView.imageTitle.text = dataSource[position].name
-            Glide.with(context).load(dataSource[position].link).into(holder.myView.image)
+            holder.myView.imageTitle.text = dataSource[position].title
+            Glide.with(context).load(dataSource[position].link).into(holder.myView.imageButton)
             if (dataSource[position].favorite!!)
                 holder.myView.likeButton.setImageResource(R.drawable.ic_like_complete)
             holder.myView.likeButton.setOnClickListener {
-
-                buttonLikeCallback(dataSource[position].id, "image")
+                if (dataSource[position].is_album!!)
+                    buttonLikeCallback(dataSource[position].id, "album")
+                else
+                    buttonLikeCallback(dataSource[position].id, "image")
                 if (holder.myView.likeButton.drawable.getConstantState()?.equals(
                         context.getResources().getDrawable(
                             R.drawable.ic_like_complete
@@ -68,6 +74,12 @@ class MyAdapterHomePage(
                 else
                     holder.myView.likeButton.setImageResource(R.drawable.ic_like_complete)
 
+            }
+            if (dataSource[position].is_album!!)
+            {
+                holder.myView.imageButton.setOnClickListener {
+                    clickOnAlbumCallback(dataSource[position].id)
+                }
             }
         }
     }
